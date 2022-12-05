41ed659ecaee2c01ebc55a625ea6083208546833
KellsCodes <nworjiifeanyi@gmail.com>
2022-12-05 22:52:29 +0000

    print matrix numbers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for item in matrix:
         for obj in item:
-            print(obj, end="\n" if obj == item[-1] else " ")
+            print("{:d}".format(obj), end="" if obj == item[-1] else " ")
+        print()
