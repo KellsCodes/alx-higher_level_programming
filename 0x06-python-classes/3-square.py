c21b1fa76f4810a9834b0be97e1a0a1967d7f2a9
KellsCodes <nworjiifeanyi@gmail.com>
2022-12-22 16:27:16 +0100

    square class with private instance attribute

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """class square that defines a square"""
+
+    def __init__(self, size=0):
+        """initialize size of square
+        Args:
+            size(init): size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size #  size of the square
+
+    def area(self):
+        """Returns the area of a number"""
+        return self.__square
