1af8feede1bed3b78d17c93089cdc6e295cb59d5
KellsCodes <nworjiifeanyi@gmail.com>
2023-02-18 15:53:56 +0000

    script that creates a table in the current database in MySQL server

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table first_table in the current database in MySQL server
-CREATE TABLE first_table(id INT, name VARCHAR(256)) IF NOT EXISTS first_table;
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
