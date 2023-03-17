6f47525bd205e66edeef3ee6d9773a7c68d3818c
KellsCodes <nworjiifeanyi@gmail.com>
2023-03-17 04:57:01 +0000

    script that prints string the number of times specified on the command argument or print message if type of argument is not a number

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const _arg = process.argv.slice(2);
 if (isNaN(Number(_arg[0]))) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	let i = 0;
-	while (i < Number(_arg[0])) {
-		console.log('C is fun');
-		i++;
-	}
+  let i = 0;
+  while (i < Number(_arg[0])) {
+    console.log('C is fun');
+    i++;
+  }
 }
