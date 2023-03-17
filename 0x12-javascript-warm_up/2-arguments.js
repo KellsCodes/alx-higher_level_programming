9b39c3b51078e88ce68c2b882bb1776abef506a9
KellsCodes <nworjiifeanyi@gmail.com>
2023-03-17 03:03:48 +0000

    semistandard syntax bug fix on the script for printing message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 const _args = process.argv.slice(2);
-if (!_args.length)
-{
-	console.log('No argument');
+if (!_args.length) {
+  console.log('No argument');
+} else if (_args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (_args.length === 1)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Arguments found');
-}
-
