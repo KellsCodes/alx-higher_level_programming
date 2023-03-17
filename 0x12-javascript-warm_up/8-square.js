73465187cd4a7d9693d1450824c3b86918ca2e9f
KellsCodes <nworjiifeanyi@gmail.com>
2023-03-17 05:41:54 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
-
+const _args = process.argv.slice(2);
+if (isNaN(Number(_args[0]))) {
+	console.log('Missing size');
+} else {
+	let i = 0, j = 0;
+	while (i < Number(_args[0])) {
+		let chars = '';
+		while (j < Number(_args[0])) {
+			chars += 'x';
+			j++;
+		}
+		console.log(chars);
+		j = 0;
+		i++;
+	}
+}
